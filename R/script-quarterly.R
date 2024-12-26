7904e0cb4feacbdf13c0ef05124e7bdbf10754a4
tomaszaba <tomas.zaba@outlook.com>
2024-12-26 17:18:30 +0200

    Address #8, #16

--- R/script-quarterly.R
+++ R/script-quarterly.R
@@ -169,3 +169,26 @@ som_admissions_quarterly |>
   ggplot(aes(x = Quarterly, y = admissions)) +
   geom_point() +
   geom_smooth(method = "loess", se = FALSE)
+
+## Specify models ----
+fit_admissions <- som_admissions_quarterly |> 
+  summarise_admissions(
+    .group = FALSE,
+    time = "Q"
+  ) |> 
+  model(
+    linear = TSLM(admissions ~ trend() + season()),
+    exp_log_trend = TSLM(admissions ~ log(trend()) + season()),
+    exp_log_admissions = TSLM(log(admissions) ~ log(trend()) + season()),
+    piecewise = TSLM(
+      log(admissions) ~ trend(knots = c(yearquarter("2020 Q1"),
+                                   yearquarter("2020 Q2"),
+                                   yearquarter("2021 Q1"),
+                                   yearquarter("2023 Q1"))) + season()
+    )
+  )
+
+### Evaluate residuals ----
+fit_admissions |> 
+  select(piecewise) |> 
+  gg_tsresiduals()

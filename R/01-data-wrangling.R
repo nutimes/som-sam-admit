2850ca8fdb83b43358caf9de983112ceac5a7b79
tomaszaba <tomas.zaba@outlook.com>
2025-01-06 16:24:25 +0200

    update the list of districts to exclude

--- R/01-data-wrangling.R
+++ R/01-data-wrangling.R
@@ -11,18 +11,40 @@ library(fable)
 # ---- Load utility functions --------------------------------------------------
 source("R/utils.R")
 
-# ---- Load data ---------------------------------------------------------------
-## Remove the "X"'s before the dates ----
-colnames(admissions) <- gsub("^X", "", colnames(admissions))
-colnames(admissions) <- gsub("\\.", "-", colnames(admissions))
+########################### FOR MONTHLY ANALYSIS ###############################
 
-########################### QUARTERLY ANALYSIS #################################
+# ---- Tidy the data -----------------------------------------------------------
+monthly_admissions <- admissions |> 
+  pivot_longer(
+    cols = !c(region, district, lsystems),
+    names_to = "time",
+    values_to = "admissions"
+  ) |> 
+  mutate(
+    time = ymd(time),
+    Monthly = yearmonth(time)
+  ) |> 
+  relocate(
+    Monthly, 
+    .before = admissions
+) |> 
+  select(-time)
+
+# ---- Remove districts with zero admissions ---------------------------------
+list <- c("Ceel_Dheere", "Jalalaqsi", "Jamaame", "Kurtunwaarey", "Sablaale", 
+"Adan Yabaal", "Bu'aale", "Jilib", "Saakow/Salagle", "Sheik", "Cadale", 
+"Xarardheere")
+
+monthly_admissions <- monthly_admissions |> 
+  filter(!(district %in% list))
+
+
+######################## FOR QUARTERLY ANALYSIS ################################
 
 # ---- Tidy the data -----------------------------------------------------------
 quarterly_admissions <- admissions |> 
-  select(2:75) |> 
   pivot_longer(
-    cols = !c(region, district),
+    cols = !c(region, district, lsystems),
     names_to = "time",
     values_to = "admissions"
   ) |> 
@@ -37,47 +59,10 @@ quarterly_admissions <- admissions |>
   select(-time)
 
 # ---- Remove districts with zero admissions -----------------------------------
-list <- c("Ceel_Dheere", "Jalalaqsi", "Sablaale", "Adan Yabaal",
- "Bu'aale", "Jilib", "Saakow/Salagle", "Sheik"
-)
+list <- c("Ceel_Dheere", "Jalalaqsi", "Jamaame", "Kurtunwaarey", "Sablaale", 
+"Adan Yabaal", "Bu'aale", "Jilib", "Saakow/Salagle", "Sheik", "Cadale", 
+"Xarardheere")
+
 quarterly_admissions <- quarterly_admissions |> 
   filter(!(district %in% list))
-
-# ---- TS Features -------------------------------------------------------------
-## Sum of admissions by Region ----
-quarterly_admissions |> 
-  summarise_admissions(
-    .group = TRUE,
-    time = "Q"
-  ) |> 
-  features(
-    .var = admissions, 
-    features = quantile
-  )
-
-############################# MONTHLY ANALYSIS #################################
-
-  monthly_admissions <- admissions |> 
-    select(2:75) |> 
-    pivot_longer(
-      cols = !c(region, district),
-      names_to = "time",
-      values_to = "admissions"
-    ) |> 
-    mutate(
-      time = ymd(time),
-      Monthly = yearmonth(time)
-    ) |> 
-    relocate(
-      Monthly, 
-      .before = admissions
-  ) |> 
-    select(-time)
-  
-  # ---- Remove districts with zero admissions ---------------------------------
-  list <- c("Ceel_Dheere", "Jalalaqsi", "Sablaale", "Adan Yabaal",
-   "Bu'aale", "Jilib", "Saakow/Salagle", "Sheik"
-  )
-  monthly_admissions <- monthly_admissions |> 
-    filter(!(district %in% list))
   

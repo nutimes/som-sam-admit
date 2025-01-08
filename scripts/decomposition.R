d9f4cf1508f59e8ff047333d63e866805eaf092c
tomaszaba <tomas.zaba@outlook.com>
2025-01-08 12:21:48 +0200

    add decomposition at national #7, #39

--- scripts/decomposition.R
+++ scripts/decomposition.R
@@ -1,23 +1,66 @@
-# ---- Decomposition -----------------------------------------------------------
-
-############################# MONTHLY ANALYSIS  ################################
-
 ################################################################################
 # As shown in the time, seasonal and subseries plot in the `eda-graphics.R` file,
 # the data shows variations that increase and decrease with the level of the serie
 # This necessitates the data to be transformed to stabelize the seasonal variation.
+
+#   Decomposition is done at national and then split into livelihood systems.
 ################################################################################
 
-## Box-Cox transformation ----
-x <- monthly_admissions |> 
+################################# NATIONAL #####################################
+
+## ---- Box-Cox transformation -------------------------------------------------
+
+### Summarise data ----
+na <- monthly_admissions |> 
+  summarise_admissions(
+    .group = FALSE,
+    time = "M"
+  )
+
+### Get lambda ----
+lambda_national <- na |> 
+  features(
+    .var = admissions,
+    features = guerrero
+  ) |> 
+  pull(lambda_guerrero)
+
+### Visualize the transformation ----
+na |> 
+  autoplot(box_cox(admissions, lambda = lambda_national))
+
+
+## ---- Decomposition ----------------------------------------------------------
+cmpnts_national <- na |> 
+  model(
+    STL(box_cox(x = admissions, lambda = lambda_national) ~ trend(window = 9) + season(window = 7))
+  ) |> 
+  components()
+
+### Visualize the components ----
+cmpnts_plot_national <- cmpnts_national |> 
+  autoplot()
+
+### Plot the seasonal component over years ----
+seasonal_cmpnt_national <- cmpnts_national |> 
+  select(season_year) |> 
+  gg_season(y = season_year)
+
+
+######################### BY LIVELIHOOD SYSTEMS ################################
+
+### Summarise data ----
+mo <- monthly_admissions |> 
   summarise_admissions(
     .group = TRUE,
     time = "M"
   )
 
-### Get a lambda value of each livelihood system time series ----
+## ---- Box-Cox transformation -------------------------------------------------
+
+### Get lambda for livelihood systems ----
 #### For Pastoral ----
-lambda_pastoral <- x |> 
+lambda_pastoral <- mo |> 
   filter(lsystems == "Pastoral") |> 
   features(
     .var = admissions,
@@ -26,7 +69,7 @@ lambda_pastoral <- x |>
   pull(lambda_guerrero)
 
 #### For Agropastoral ----
-lambda_agropastoral <- x |> 
+lambda_agropastoral <- mo |> 
   filter(lsystems == "Agropastoral") |> 
   features(
     .var = admissions,
@@ -35,7 +78,7 @@ lambda_agropastoral <- x |>
   pull(lambda_guerrero)
 
 #### For Riverine ----
-lambda_rivernine <- x |> 
+lambda_rivernine <- mo |> 
   filter(lsystems == "Riverine") |> 
   features(
     .var = admissions,
@@ -44,7 +87,7 @@ lambda_rivernine <- x |>
   pull(lambda_guerrero)
 
 #### For Urban/IDP's ----
-lambda_urban_idps <- x |> 
+lambda_urban_idps <- mo |> 
   filter(lsystems == "Urban/IDPs") |> 
   features(
     .var = admissions,
@@ -55,28 +98,30 @@ lambda_urban_idps <- x |>
 
 ## Visualize the time series after transformation ----
 ### Pastoral ----
-x |> 
+mo |> 
   filter(lsystems == "Pastoral") |> 
   autoplot(box_cox(admissions, lambda = lambda_pastoral))
 
 ### Agropastoral ----
-x |> 
+mo |> 
   filter(lsystems == "Agropastoral") |> 
   autoplot(box_cox(admissions, lambda = lambda_agropastoral))
 
 ### Riverine ----
-x |> 
+mo |> 
   filter(lsystems == "Riverine") |> 
   autoplot(box_cox(admissions, lambda = lambda_rivernine))
 
 ### Urban/IDPs ----
-x |> 
+mo |> 
   filter(lsystems == "Urban/IDPs") |> 
   autoplot(box_cox(admissions, lambda = lambda_urban_idps))
 
-## Decompose with STL ----
+
+## ---- Decomposition ----------------------------------------------------------
+
 ### Pastoral ----
-cmpnts_pastoral <- x |> 
+cmpnts_pastoral <- mo |> 
   filter(lsystems == "Pastoral") |> 
   model(
     STL(box_cox(admissions, lambda_pastoral) ~ trend(window = 9) + season(window = 7))
@@ -85,15 +130,15 @@ cmpnts_pastoral <- x |>
 
 ### Visualize the components ----
 cmpnts_plot_pastoral <- cmpnts_pastoral |> 
-  autoplot() +
+  autoplot()
 
-### Seasonal component over years ----
+### Plot the seasonal component over years ----
 seasonal_cmpnt_pastoral <- cmpnts_pastoral |> 
   select(season_year) |> 
   gg_season(y = season_year)
 
 ### Agropastoral ----
-cmpnts_agropastoral <- x |> 
+cmpnts_agropastoral <- mo |> 
   filter(lsystems == "Agropastoral") |> 
   model(
     STL(box_cox(admissions, lambda_agropastoral) ~ trend(window = 9) + season(window = 7))
@@ -103,13 +148,13 @@ cmpnts_agropastoral <- x |>
 ### Visualize the components ----
 cmpnts_plot_agropastoral <- autoplot(cmpnts_agropastoral)
 
-### Seasonal component over years ----
+### Plot the seasonal component over years ----
 seasonal_cmpnt_agropastoral <- cmpnts_agropastoral |> 
   select(season_year) |> 
   gg_season(y = season_year)
 
 ### Riverine ----
-cmpnts_riverine <- x |> 
+cmpnts_riverine <- mo |> 
   filter(lsystems == "Riverine") |> 
   model(
     STL(box_cox(admissions, lambda_rivernine) ~ trend(window = 9) + season(window = 7))
@@ -119,13 +164,13 @@ cmpnts_riverine <- x |>
 ### Visualize the components ----
 cmpnts_plot_riverine <- autoplot(cmpnts_riverine)
 
-### Seasonal component over years ----
+### Plot the seasonal component over years ----
 seasonal_cmpnt_riverine <- cmpnts_riverine |> 
   select(season_year) |> 
   gg_season(y = season_year)
 
 ### Urban/IDPs ----
-cmpnts_urban_idps <- x |> 
+cmpnts_urban_idps <- mo |> 
   filter(lsystems == "Urban/IDPs") |> 
   model(
     STL(box_cox(admissions, lambda_urban_idps) ~ trend(window = 9) + season(window = 7))
@@ -135,7 +180,7 @@ cmpnts_urban_idps <- x |>
 ### Visualize the components ----
 cmpnts_plot_urban_idps <- autoplot(cmpnts_urban_idps)
 
-### Seasonal component over years ----
+### Plot the seasonal component over years ----
 seasonal_cmpnt_urban_idps <- cmpnts_urban_idps |> 
   select(season_year) |> 
   gg_season(y = season_year)
